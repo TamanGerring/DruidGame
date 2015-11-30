30052a6c1bfece300616ac171dd58e36d4316f4b
Arayian <arayian@hotmail.com>
2015-11-30 01:33:14 -0500

    Stuff

--- Game1/PenaltyObject.cs
+++ Game1/PenaltyObject.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,5 +15,29 @@ namespace Game1
         --Value
         --Sprite
         */
+
+        private ContentManager _contentManager;
+        private Texture2D _sprite;
+        private int _radius;
+        private Vector2 _position;
+        private Vector2 _center;
+
+        public PenaltyObject(
+    ContentManager contentManager,
+    string spriteName,
+    int radius,
+    Vector2 position
+    )
+        {
+            _contentManager = contentManager;
+            _spriteName = spriteName;
+            _radius = radius;
+            _position = position;
+            _center = position + new Vector2(radius, radius);
+
+            // load the ball image into the Texture2D for the ball sprite
+            _sprite = _contentManager.Load<Texture2D>(_spriteName);
+        }
     }
 }
+
--- Game1/ScoringObjects.cs
+++ Game1/ScoringObjects.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,5 +15,46 @@ namespace Game1
         --Placement Method
         --Removal Method
         */
+
+        private ContentManager _contentManager;
+        private Texture2D _sprite;
+        private int _radius;
+        private Vector2 _position;
+        private Vector2 _center;
+
+        public ContentManager ContentManager
+        {
+            get { return _contentManager; }
+            set { _contentManager = value; }
+        }
+
+        public Texture2D Sprite
+        {
+            get { return _sprite; }
+            set { _sprite = value; }
+        }
+
+        public int Radius
+        {
+            get { return _radius; }
+            set { _radius = value; }
+        }
+
+        public Vector2 Position
+        {
+            get { return _position; }
+            set
+            {
+                _position = value;
+                _center.X = _position.X + _radius;
+                _center.Y = _position.Y + _radius;
+            }
+        }
+
+        public Vector2 Center
+        {
+            get { return _center; }
+            set { _center = value; }
+        }
     }
 }
